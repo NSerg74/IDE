da1b3b883be7fb4b3e3b1668a329d211b26a9721
NSerg74 <nserg@dataok.ru>
2025-01-04 13:04:20 +0500

    небольшие правки

--- game.py
+++ game.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
 number = np.random.randint(1,101)   #загадываем число от 1 до 100
 total_count = 0                     #переменная для подсчета общего кол циклов подбора, 1 вариант подсчета
 list_count = []                     #список, где элементы это кол за циклов за каждый проход цикла игры
-value_interation = 1
-#value_interation = int(input ('Сколь раз, вы бы хотели, чтобы сыграл копмьютер сам с собой:'))
+#value_interation = 1
+value_interation = int(input ('Сколь раз, вы бы хотели, чтобы сыграл копмьютер сам с собой:'))
 
 for i in range (value_interation):
     count = 0                       #подсчет за сколько проходов мы подобрали число, именно в этой интерации
@@ -32,13 +32,13 @@ for i in range (value_interation):
 mean_total = total_count/value_interation       #считаем по первому варианту, всего кол на кол раз игры
 mean = np.mean(list_count)                      #считаем по втрому варианту, медиана из списка
     
-#print ('Первый вариант подсчета:')
-#print (f'Total_count: {total_count}, Value_interation: {value_interation}')
-#print (f'Всего было сыграно {value_interation} партий. Компьютер в среднем подобрал число за {(mean_total)} циклов')
+print ('Первый вариант подсчета:')
+print (f'Total_count: {total_count}, Value_interation: {value_interation}')
+print (f'Всего было сыграно {value_interation} партий. Компьютер в среднем подобрал число за {(mean_total)} циклов')
 
-#print ('Второй вариант подсчета:')
-#print (f'Длина списка=кол игр: {len(list_count)}, медиана списка: {mean}')
-#print (f'Всего было сыграно {value_interation} партий. Компьютер в среднем подобрал число за {(mean)} циклов')
+print ('Второй вариант подсчета:')
+print (f'Длина списка=кол игр: {len(list_count)}, медиана списка: {mean}')
+print (f'Всего было сыграно {value_interation} партий. Компьютер в среднем подобрал число за {(mean)} циклов')
 
 
 
